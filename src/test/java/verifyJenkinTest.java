6e109df2f28b3f8a4bfc3ee02cc32b58533f2f6d
RashmiBhat <rbhat@tekion.com>
2023-08-31 15:07:29 +0530

    first commit

---
+++ src/test/java/verifyJenkinTest.java
@@ -0,0 +1,65 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class verifyJenkinTest {
+    @Test
+    public void test1()
+    {
+        System.out.println("test1");
+        Assert.assertTrue(true);
+    }
+    @Test
+    public void test2()
+    {
+        System.out.println("test2");
+        Assert.assertTrue(true);
+    }
+    @Test
+    public void test3()
+    {
+        System.out.println("test3");
+        Assert.assertTrue(true);
+    }
+    @Test
+    public void test4()
+    {
+        System.out.println("test4");
+        Assert.assertTrue(true);
+    }
+    @Test
+    public void test5()
+    {
+        System.out.println("test5");
+        Assert.assertTrue(true);
+    }
+    @Test
+    public void test6()
+    {
+        System.out.println("test6");
+        Assert.assertTrue(true);
+    }
+    @Test
+    public void test7()
+    {
+        System.out.println("test7");
+        Assert.assertTrue(true);
+    }
+    @Test
+    public void test8()
+    {
+        System.out.println("test8");
+        Assert.assertTrue(true);
+    }
+    @Test
+    public void test9()
+    {
+        System.out.println("test9");
+        Assert.assertTrue(true);
+    }
+    @Test
+    public void test10()
+    {
+        System.out.println("test10");
+        Assert.assertTrue(true);
+    }
+}
